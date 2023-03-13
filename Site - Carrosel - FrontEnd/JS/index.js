85e3039046744cabfb6d2290efdb6d4e6d388aba
Wallace Ribeiro <wallacesantos12wr@gmail.com>
2023-03-13 12:16:15 -0300

    Projeto: Site Web temático com carrossel de imagens
    Ferramentas utilizadas no desenvolvimento: HTML5, CSS3 e JavaScript.

---
+++ Site - Carrosel - FrontEnd/JS/index.js
@@ -0,0 +1,23 @@
+// Página JS
+//-----------------------------------------------------------------------------
+	const botoesCarrossel = document.querySelectorAll('.botao');
+	const imagens = document.querySelectorAll('.imagem')
+//-----------------------------------------------------------------------------
+		botoesCarrossel.forEach((botao, indice) => {
+			botao.addEventListener('click', () => {
+//-----------------------------------------------------------------------------				
+				console.log(indice);
+//-----------------------------------------------------------------------------				
+				const botaoSelecionado = document.querySelector('.selecionado');
+				botaoSelecionado.classList.remove('selecionado');
+//-----------------------------------------------------------------------------				
+				botao.classList.add('selecionado');
+//-----------------------------------------------------------------------------				
+				const imagemAtiva = document.querySelector('.ativa');
+				imagemAtiva.classList.remove('ativa');
+//-----------------------------------------------------------------------------				
+				console.log(imagens);
+				imagens[indice].classList.add('ativa');
+//-----------------------------------------------------------------------------
+	})
+})
